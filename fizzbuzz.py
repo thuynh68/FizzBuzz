cc1f3d194863ae6c4645c8ae5ece4e0fe2bd6b5e
Tri Huynh <thuynh68@hotmail.com>
2014-11-08 22:23:50 +0000

    Finished with changes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,12 @@
-print "Fizz buzz counting up to 100"
-upperLimit = 101
-for i in range(1,upperLimit):
+import sys
+upperLimit = 100
+if len(sys.argv) > 1:
+    upperLimit = int(sys.argv[1])
+else:
+    upperLimit = int(raw_input("Enter upper limit: "))
+    
+print "Fizz buzz counting up to {}".format(upperLimit)
+for i in range(1,upperLimit+1):
     divisibleByThree = i % 3 == 0
     divisibleByFive = i % 5 == 0     
     if (not divisibleByThree) and (not divisibleByFive):
